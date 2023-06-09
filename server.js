99371cd7a7559b64c3c1e7e315252ee79bfacce0
Formation20 <sheeladityamohanty@gmail.com>
2023-06-09 13:39:49 +0530

    sheeladitya

--- server.js
+++ server.js
@@ -14,3 +14,17 @@ app.use(express.static(__dirname + '/public'))
 app.get ('/',(req,res) => {
     res.sendFile(__dirname + '/index.html')
 })
+
+
+//socket
+
+const io = require ('socket.io')(http)
+
+io.on ('connection', (socket) => {
+    console.log('Connected...')
+
+socket.on('message', (msg) => {
+socket.broadcast.emit('message', msg )
+})
+
+})
